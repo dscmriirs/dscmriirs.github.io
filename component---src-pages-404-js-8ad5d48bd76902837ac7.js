c1142404a15888ad5a3ea45667c7d7b553296e9d
pranav2012 <p2012agarwal@gmail.com>
2021-08-06 02:36:37 +0530

    Updates

---
+++ component---src-pages-404-js-8ad5d48bd76902837ac7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),r=a("Bl7J"),c=a("7vrA"),o=a("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("p",null,"It looks like this page doesn't exist."),l.a.createElement("p",null,"Click ",l.a.createElement("a",{href:"/"},"here")," to go back")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8ad5d48bd76902837ac7.js.map
